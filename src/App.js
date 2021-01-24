0775b73da5329a56ca97abf08211432087fe4792
giacomo9999 <gmarchesi888@gmail.com>
2021-01-24 16:52:50 -0500

    My suspicions are confirmed. Or else I'm just dumb.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
+import React, { Component } from "react";
+
 function App() {
+  const [count, setCount] = React.useState(0);
+
   return (
-    <div data-test="app-container" className="container-outer">
-      <h1>APP</h1>
+    <div data-test="component-app" className="container-outer">
+      <h1 data-test="counter-display">
+        {`Counter: `}
+        <span data-test="count">{count}</span>
+      </h1>
+      <button
+        data-test="increment-button"
+        onClick={() => {
+          setCount(count + 1);
+          console.log("Count: ", count);
+        }}
+      >
+        Click Me
+      </button>
     </div>
   );
 }
+
+// class App extends Component {
+//   state = { count: 0 };
+
+//   render() {
+//     return (
+//       <div data-test="component-app" className="container-outer">
+//         <h1 data-test="counter-display">
+//           {`Counter: `}
+//           <span data-test="count">{this.state.count}</span>
+//         </h1>
+//         <button
+//           data-test="increment-button"
+//           onClick={() => {
+//             this.setState({ count: this.state.count + 1 });
+//           }}
+//         >
+//           Click Me
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,3 @@
-import { render, screen } from "@testing-library/react";
 import React from "react";
 import App from "./App";
 import Enzyme, { shallow } from "enzyme";
@@ -6,7 +5,19 @@ import EnzymeAdapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
-test("renders App", () => {
-  const wrapper = shallow(<App />);
-  // expect(wrapper).toBeInTheDocument();
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+test("counter starts at 0", () => {
+  const count = findByTestAttr(setup(), "count").text();
+  expect(count).toBe("0");
+});
+
+test("clicking on button increments counter display", () => {
+  const button = findByTestAttr(setup(), "increment-button");
+  // console.log("Button:", button.debug());
+  button.simulate("click");
+  const count = findByTestAttr(setup(), "count").text();
+  expect(count).toBe("1");
 });
